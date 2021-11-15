83391af509cba7cc1ffacbd7287c384fb25c89e9
Mikhail Rukavishnikov <61332555+rukavishnikovmihail00@users.noreply.github.com>
2021-11-15 19:02:56 +0300

    update

--- zadacha.py
+++ zadacha.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import itertools
 
 
 def init_logger(debug : False):
@@ -51,35 +52,92 @@ def calculate_loads_weight(load_unit_weight, A):
     logging.debug("-----------------------")
     logging.info(f"Единиц груза для погрузки всего: {total_weight_start}\n")
 
-
-    for item in remain:
-        if item[-1] != 0:
-            logging.debug(f"{item[-1]} единиц товара из {item[0]} вида грузов разложены по {len(load_unit_weight) - len(arr)} оставшимся контейнерам")
-           
-    
-    total_weight = 0
-
-    for item in arr:
-        total_weight += item[-1]
-
-    logging.debug(f"{total_weight} единиц груза полностью заполнили первые {len(arr)} контейнеров")
-
-
     logging.info("\n======= Оптимальное распределение грузов =======\n")
     shipment = []
 
     new_arr = arr.copy()
 
+
+    ship_num = 0
+    cont_num = 0
     for i in range(n):
         for j in range(m):
             if new_arr:
                 shipment.append([f"Корабль {str(i+1)}, контейнер {str(j+1)}: груз вида {str(new_arr[0][0])} = {str(new_arr[0][1])} единиц"])
                 new_arr.pop(0)
+                cont_num = j + 1
+                ship_num = i + 1
     for el in shipment:
         logging.info(el[0])
-    # ДОДЕЛАТЬ С ПОСЛЕДНИМИ КОНТЕЙНЕРАМИ
+    print(ship_num)
+    print(cont_num)
+
+    
+    new_remain = []
+    new_remain_to_load = []
+
+    for el in remain:
+        if el[1] != 0:
+            #print(el)
+            new_remain_to_load.append(el)
+    for i, item in enumerate(load_unit_weight):
+        if item != -1:
+            new_remain.append(item)
+            print(f"item = {item}, {i}")
+
+
+
+    # ============
+    print(new_remain_to_load)
+    print(new_remain)
+    
+    print(f"shipment = {shipment}")
+    
+
+    for i, el in enumerate(new_remain_to_load):
+        weight_remain = el[1]
+        while weight_remain != 0:
+            for i, item in enumerate(new_remain):
+                if weight_remain >= item:
+                    if item != -1:
+                        weight_remain = weight_remain - item
+                        el[1] = weight_remain
+                        print(f"Груз вида {el[0]} весом {item} кладем в контейнер {item}")
+                        print(new_remain_to_load)
+                        new_remain[i] = -1
+                        print(new_remain)
+                else:
+                    if item != -1:
+                        print("else")
+                        new_remain[i] -= weight_remain
+                        print(f"Груз вида {el[0]} весом {weight_remain} кладем в контейнер {item}")
+                        weight_remain = 0
+                        el[1] = weight_remain
+                        print(new_remain_to_load)
+                        print(new_remain)
+                        break
+                        
+
+    print("------------------")
+    print(new_remain_to_load)
+    print(new_remain)
+
+    for item in new_remain_to_load:
+        logging.debug(f"{item[-1]} единиц товара из {item[0]} вида грузов разложены по {len(load_unit_weight) - len(arr)} оставшимся контейнерам")
+    
+    
     logging.info("=================================================\n")
 
+    #==================
+
+    total_weight = 0
+
+    #for item in arr:
+    #    total_weight += item[-1]
+
+    #logging.debug(f"{total_weight} единиц груза полностью заполнили первые {len(arr)} контейнеров")
+
+    exit(0)
 
 def A_to_X(A, D):
     total = sum(A)
@@ -144,7 +202,7 @@ if __name__=="__main__":
     init_logger(debug)
     
 
-    ENV_FILE = "env1.json"
+    ENV_FILE = "env.json"
 
     with open(ENV_FILE, 'r') as r_file:
         data = json.load(r_file)
